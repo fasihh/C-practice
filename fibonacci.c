a7abaea748d2605f57c3b9add294b4260928586c
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-18 22:44:27 +0500

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fibonacci(int, int, int, int);
+
+void main() {
+	fibonacci(1, 1, 0, 45);
+
+	printf("\n");
+}
+
+int fibonacci(int num1, int num2, int answer, int n) {
+	if (n) {
+		printf("%d  ", answer);
+		return fibonacci(num2, answer, answer + num2, n - 1);
+	}
+	printf("\nphi: %f", (float) answer/num2);
+	return n;
+}
+
+// 1 1 2 3 5 8
