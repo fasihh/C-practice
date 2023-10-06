f054b234acf93ecbefb60a40425c0efe9c624226
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-10-06 22:44:24 +0500

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <conio.h>
 
 #define CH '.'
 
@@ -47,7 +47,7 @@ bool checkPos(char *grid, int *pos) {
 }
 
 char getTurn(int turn) {
-	return (turn & 1) ? 'x' : 'o';
+	return (turn & 1) ? 'o' : 'x';
 }
 
 void setValue(char* grid, int *pos, int turn) {
@@ -55,68 +55,93 @@ void setValue(char* grid, int *pos, int turn) {
 	*(grid + pos[0]*3 + pos[1]) = ch;
 }
 
-bool checkHorizontal(char *graph, int row, char ch) {
+bool checkVertical(char *grid, char ch) {
+	int c;
 	for (int i = 0; i < 3; i++) {
-		if (*(graph + row*3 + i) != ch) {
-			return false;
+		c = 0;
+
+		for (int j = 0; j < 3; j++) {
+			if (*(grid + j*3 + i) != ch) {
+				break;
+			}
+			c++;
 		}
+
+		if (c == 3) return true;
 	}
-
-	return true;
-}
-
-bool checkVertical(char *graph, int col, char ch) {
-	for (int i = 0; i < 3; i++) {
-		if (*(graph + i*3 + col) != ch) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
-bool checkDiagonal(char *grid, char ch) {
-	for (int i = 0; i < 3; i++) {
-		if (*(grid + i*3 + i) != ch) {
-			return false;
-		}
-	}
-
-	for (int i = 0, j = 2; i < 3; i++, j--) {
-		if (*(grid + j*3 + i) != ch) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
-// game logic remaining 
-bool checkStatus(char *grid, int turn) {
 	
-	return true;
+
+	return false;
 }
+
+bool checkHorizontal(char *grid, char ch) {
+	int c;
+	for (int i = 0; i < 3; i++) {
+		c = 0;
+
+		for (int j = 0; j < 3; j++) {
+			if (*(grid + i*3 + j) != ch) {
+				break;
+			}
+			c++;
+		}
+
+		if (c == 3) return true;
+	}
+
+	return false;
+}
+
+bool checkDiagonal(char *grid, int diag[3][2], char ch) {
+	int c = 0;
+	for (int i = 0; i < 3; i++) {
+		if (*(grid + diag[i][0]*3 + diag[i][1]) != ch) {
+			break;
+		}
+
+		c++;
+	}
+
+	return (c == 3) ? true : false;
+}
+
+bool checkStatus(char *grid, int turn) {
+	int diag1[3][2] = {{0, 0}, {1, 1}, {2, 2}};
+	int diag2[3][2] = {{2, 0}, {1, 1}, {0, 2}};
+	char ch = getTurn(turn);
+
+	return (checkVertical(grid, ch) || checkHorizontal(grid, ch) || checkDiagonal(grid, diag1, ch) || checkDiagonal(grid, diag2, ch) || turn >= 8) ? false : true;
+} 
 
 int main() {
+	bool status = true;
 	char posInput[2];
 	int turn = 0;
 	int *pos;
 	char *grid = createGrid();
 	displayGrid(grid);
 	
-	while (checkStatus(grid, turn)) {
+	while (status) {
+		printf("%d\n", turn);
 		do {
-			printf("Enter position: ");
+			printf("\nEnter position '%c': ", getTurn(turn));
 			gets(posInput);
 			pos = getPos(posInput);
 
 		} while (!checkPos(grid, pos));
 
-		//printf("%d\n", checkDiagonal(grid, getTurn(turn)));
 		setValue(grid, pos, turn);
 		displayGrid(grid);
+		status = checkStatus(grid, turn);
 		turn++;
 	}
 
+	if (turn >= 8) {
+		printf("draw!");
+	} else {
+		printf("\n'%c' won!", getTurn(turn - 1));
+	}
+	
+	getch();
 	return 0;
 }
